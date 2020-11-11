df6d927321e707183eaf2f35d6fe5cef81164c5d
EgorTarasov <tarasov.egor.yandex@gmail.com>
2020-11-12 00:07:16 +0300

    2560x1440 fix

--- mouse.py
+++ mouse.py
@@ -5,13 +5,19 @@ import pyautogui as gui
 from time import sleep
 
 URL_AUTH = 'https://login.mos.ru/sps/oauth/ae?scope=openid+profile+blitz_user_rights+snils+contacts+blitz_change_password&access_type=offline&response_type=code&redirect_uri=https://dnevnik.mos.ru/sudir&state=6b0506f5-43fd-41ac-9a43-088375815733&client_id=dnevnik.mos.ru'
-lessons = [[1600, 520], [1600, 645], [1600, 770], [1600, 895], [1600, 1020], [1600, 1145]]
+lessons = [[1550, 590], [1600, 715], [1550, 715], [1550, 965], [1550, 1090], [1550, 1145]]
 size = gui.size()
-teams_btn = [1345, 385]
+teams_btn = [1315, 465]
+teams_cancel_btn = [1145, 340]
+teams_continue_btn = [1295, 725]
+mic_btn = [990, 930]
+webcam_btn = [890, 930]
+name_textfield = [900, 875]
+teams_join_btn = [1125, 875]
 
 
 def open_chrome():
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    chrome_path = '/usr/bin/google-chrome %s'
     webbrowser.get(chrome_path).open(URL_AUTH)
 
 
@@ -20,40 +26,45 @@ def get_click():
 
 
 def auth():
-    sleep(2)
-    gui.click([1030, 560])
+    sleep(7)
+    gui.click([990, 630])
 
 
 def enter_lesson():
-    sleep(3)
+    sleep(1)
     gui.leftClick(teams_btn)
-    sleep(5)
-    gui.leftClick(1055, 260)
-    sleep(3)
-    gui.leftClick(1220, 560)
+    sleep(1)
+    gui.leftClick(teams_cancel_btn)
+    sleep(1)
+    gui.leftClick(teams_continue_btn)
     sleep(7)
 
     # camera off
-    gui.leftClick(920, 620)
+    gui.leftClick(webcam_btn)
     sleep(0.6)
     # mic off
-    gui.leftClick(1015, 620)
+    gui.leftClick(mic_btn)
     sleep(0.6)
 
     # entering name
-    gui.doubleClick(900, 575)
-    gui.write('Tujh Nfhfcjd', interval=0.1)
+    '''
+    gui.doubleClick(name_textfield)
+    gui.hotkey('control', 'a')
+    gui.press('backspace')
+    gui.hotkey('win', 'space')
+    gui.write('Name Surname', interval=0.1)
+    '''
 
     # entering meeting
-    gui.leftClick(1175, 575)
+    gui.leftClick(teams_join_btn)
 
 
 def main():
+    # TODO: Поздороваться и вырубить микро
+    # TODO: Сервер с записями голосов
+    # TODO:
     get_click()
     open_chrome()
-    #gui.hotkey('control', 'command', 'f')
-    sleep(5)
-    auth()
     sleep(7)
 
     '''Accesing first lesson'''
@@ -62,4 +73,6 @@ def main():
 
 
 if __name__ == '__main__':
+    #gui.moveTo(teams_btn)
     main()
+    #get_click()
